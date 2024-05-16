ffa3adbbb25634326ba8d23427a11d1e62698e4a
TigaByte <jonathan@tigabyte.de>
2024-05-16 09:51:11 +0200

    WindowManager init() added

---
+++ src/main/java/de/blackbird/Launcher.java
@@ -0,0 +1,13 @@
+package de.blackbird;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        System.out.println(Version.getVersion());
+
+    }
+}
+
